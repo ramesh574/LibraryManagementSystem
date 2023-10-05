f106be0f2c1282f13115ddda9bee07adce9eea15
ramesh574 <rames15698@gmail.com>
2023-10-05 11:55:08 +0530

    This is Review Repository

--- backend/library-world/src/main/java/com/ramesh/libraryworld/dao/ReviewRepository.java
+++ backend/library-world/src/main/java/com/ramesh/libraryworld/dao/ReviewRepository.java
@@ -1,6 +1,7 @@
 package com.ramesh.libraryworld.dao;
 
 import com.ramesh.libraryworld.entity.Review;
+import lombok.Data;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
