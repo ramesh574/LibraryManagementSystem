b02d3e458af993c97cd86d08c1967dc6cbd448a3
ramesh574 <rames15698@gmail.com>
2023-10-05 11:56:15 +0530

    This is Review Service

---
+++ backend/library-world/src/main/java/com/ramesh/libraryworld/service/ReviewService.java
@@ -0,0 +1,52 @@
+package com.ramesh.libraryworld.service;
+
+import com.ramesh.libraryworld.dao.BookRepository;
+import com.ramesh.libraryworld.dao.ReviewRepository;
+import com.ramesh.libraryworld.entity.Review;
+import com.ramesh.libraryworld.requestmodels.ReviewRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Service
+@Transactional
+public class ReviewService {
+    private BookRepository bookRepository;
+    private ReviewRepository reviewRepository;
+    @Autowired
+    public ReviewService(BookRepository bookRepository,ReviewRepository reviewRepository)
+    {
+        this.bookRepository=bookRepository;
+        this.reviewRepository=reviewRepository;
+    }
+    public void postReview(String userEmail, ReviewRequest reviewRequest) throws Exception {
+        Review validateReview=reviewRepository.findByUserEmailAndBookId(userEmail,reviewRequest.getBookId());
+        if(validateReview!=null){
+            throw new Exception("Review already created");
+        }
+        Review review=new Review();
+        review.setBookId(reviewRequest.getBookId());
+        review.setRating(review.getRating());
+        review.setUserEmail(userEmail);
+        if(reviewRequest.getReviewDescription().isPresent())
+        {
+            review.setReviewDescription(reviewRequest.getReviewDescription().map(
+                    Object::toString).orElse(null)
+            );
+        }
+        review.setDate(Date.valueOf(LocalDate.now()));
+        reviewRepository.save(review);
+    }
+    public Boolean userReviewListed(String userEmail, Long bookId) {
+        Review validateReview = reviewRepository.findByUserEmailAndBookId(userEmail, bookId);
+        if (validateReview != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
