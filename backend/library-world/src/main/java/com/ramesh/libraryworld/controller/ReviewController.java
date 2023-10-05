a1d9e270ab4e9a0ebe37dd0b87e9c1dce2f22f86
ramesh574 <rames15698@gmail.com>
2023-10-05 11:54:36 +0530

    I added ReviewController

---
+++ backend/library-world/src/main/java/com/ramesh/libraryworld/controller/ReviewController.java
@@ -0,0 +1,36 @@
+package com.ramesh.libraryworld.controller;
+
+import com.ramesh.libraryworld.requestmodels.ReviewRequest;
+import com.ramesh.libraryworld.service.ReviewService;
+import com.ramesh.libraryworld.utils.ExtractJWT;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+    private ReviewService reviewService;
+    public ReviewController(ReviewService reviewService)
+    {
+        this.reviewService=reviewService;
+    }
+    @GetMapping("/secure/user/book")
+    public Boolean reviewBookByUser(@RequestHeader(value="Authorization") String token,
+                                    @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+
+        if (userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+        return reviewService.userReviewListed(userEmail, bookId);
+    }
+    @PostMapping("/secure")
+    public void postReview(@RequestHeader(value="Authorization") String token,
+                           @RequestBody ReviewRequest reviewRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if (userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+        reviewService.postReview(userEmail, reviewRequest);
+    }
+}
